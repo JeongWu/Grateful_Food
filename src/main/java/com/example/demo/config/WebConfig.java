8dc2dc462f0507195520786d0a893cdac229e0b1
jeonhyeonu <tkawnd111@gmail.com>
2020-04-14 22:00:03 +0900

    CORS, Cross Origin Resource Sharing

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -5,6 +5,7 @@ import com.example.demo.config.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
@@ -21,4 +22,12 @@ public class WebConfig implements WebMvcConfigurer { //handlerMethodArgumentReso
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
         argumentResolvers.add(loginUserArgumentResolver);
     }
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry
+                .addMapping("/api/**")
+                .allowedOrigins("http://locallhost:3000");
+    }
 }
+
+
